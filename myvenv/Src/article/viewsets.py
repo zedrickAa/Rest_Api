d0fac006c21a9fb9806f1b988c914b283c3242d0
zedrick huinac <zedrickhuinac.j@gmail.com>
2019-10-22 22:50:40 -0600

    first commit

---
+++ myvenv/Src/article/viewsets.py
@@ -0,0 +1,6 @@
+from rest_framework import viewsets
+from .models import Article
+from .serializers import ArticleSerializer
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
